75ad4eb7fde954bdfbba9e7f10ed742e28d91e24
Grégoire Boisseau <boisseaugregoire@gmail.com>
2019-12-13 11:49:44 +0100

    Test pour la sécurisation des pages en back

--- controllers/backController.php
+++ controllers/backController.php
@@ -1,31 +1,35 @@
 <?php
-    namespace App\Controller;
 
-    use App\Model\AnnonceModel as AnnonceModel;
-    use App\Pictures\Pictures as Pictures;
-    use App\View\View as View;
-    use Exception;
+namespace App\Controller;
 
-    class backController
+use App\Model\AnnonceModel as AnnonceModel;
+use App\Pictures\Pictures as Pictures;
+use App\View\View as View;
+use Exception;
+
+class backController
+{
+
+    public function addAnnonce()
     {
-
-        public function addAnnonce()
-        {
+        if (is_connected()) {
             $v = new View("Ajouter une annonce", "layout", "annonce/add_annonce.php");
-            $v -> requireView();
+            $v->requireView();
+        } else {
+            throw new Exception();
         }
-
-        public function addAnnoncePost()
-        {
-            extract($_POST);
-            
-            $am = new AnnonceModel();
-            $id = $am->addAnnonce($titre, $texte, $prix);
-            
-            $photos_bdd = Pictures::addPictures($_FILES, "public/img/annonce", "annonce_", $id);
-            $am -> addPhotos($id, $photos_bdd);
-
-            header("Location: /annonce/$id");
-        }
-
     }
+
+    public function addAnnoncePost()
+    {
+        extract($_POST);
+
+        $am = new AnnonceModel();
+        $id = $am->addAnnonce($titre, $texte, $prix);
+
+        $photos_bdd = Pictures::addPictures($_FILES, "public/img/annonce", "annonce_", $id);
+        $am->addPhotos($id, $photos_bdd);
+
+        header("Location: /annonce/$id");
+    }
+}
